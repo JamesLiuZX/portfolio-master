b90fc2f1ed82499f20a43699b0f8be6cb7d9817d
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-01-20 18:13:51 +0800

    Created init MVP

---
+++ data/content/projects.ts
@@ -0,0 +1,66 @@
+import { kebabCase, kebabArray } from "@/utils/utils";
+import { Project } from "types";
+
+const projects: Project[] = [
+  {
+    id: 0,
+    title: "NFInsighT",
+    desc: "All-in-one NFT Dashboard for Price Prediction, Analytics and Sentiments. Consisting over 700 NFT collections in database and complete with account authentication and wallet integration.",
+    img: "/static/projects/yei-learn.png",
+    link: "https://nfinsight.vercel.app/",
+    github: "https://github.com/SeeuSim/dogehalla",
+    tags: ["Typescript", "NextJS", "TailwindCSS", "Prisma"],
+  },
+  {
+    id: 1,
+    title: "Dunman Helper",
+    desc: "A simple and elegant chatbot for anyone interested in finding out more about Dunman High School. ",
+    img: "/static/projects/buildfaster.png",
+    link: "https://jamesliuzx.github.io/AI-Chatbot/",
+    tags: ["HTML", "CSS", "Javascript", "Python"],
+  },
+  {
+    id: 2,
+    title: "Portfolio Site",
+    desc: "This current site/",
+    img: "/static/projects/react-emoji-search.png",
+    link: "localhost:3000",
+    tags: ["Typescript", "React", "NextJS", "TailwindCSS","Vercel"],
+  },
+  {
+    id: 3,
+    title: "Netinder",
+    desc: "A dating app in a Netflix format",
+    img: "/static/projects/create-html-boilerplate.png",
+    tags: ["React", "Node"],
+  },
+  {
+    id: 4,
+    title: "MetaTrader4 Expert Advisors",
+    desc: "Wrote scripts to automate trading based on customisable parameters",
+    img: "/static/projects/create-html-boilerplate.png",
+    github: "https://github.com/JamesLiuZX/MetaTrader4",
+    tags: ["MQL4", "C++"],
+  },
+  {
+    id: 5,
+    title: "Shopify E-Commerce Site",
+    desc: "Designed website for business, with over 300 unique visitors per month. Added code customisations for customer needs.",
+    img: "/static/projects/create-html-boilerplate.png",
+    link: "https://herbalbathsg.com",
+    tags: ["Shopify", "HTML", "CSS", "Javascript"],
+  },
+
+];
+
+export const allTags = []
+
+projects.forEach((project) => {
+  project.tags.forEach((tag) => !allTags.includes(tag) && allTags.push(tag))
+});
+
+export const allKebabTags = allTags.map(tag => (
+  kebabCase(tag)
+))
+
+export default projects
---
+++ data/global.ts
@@ -0,0 +1,95 @@
+type Route = {
+  title: string,
+  path: string
+}
+
+type FooterCol = {
+  title: string,
+  links: {
+    name: string,
+    link: string,
+    icon?: string,
+    leavesWebsite: boolean
+  }[]
+}
+
+type Footer = {
+  columns: FooterCol[]
+  support: {
+  }
+};
+
+export const routes: Route[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  // {
+  //   title: "Blog",
+  //   path: "/blog",
+  // },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Designs",
+    path: "/designs",
+  },
+];
+
+
+export const footer: Footer = {
+  columns: [
+    {
+      title: "Pages",
+      links: [
+        {
+          name: "Home",
+          link: "/",
+          leavesWebsite: false,
+        },
+        // {
+        //   name: "Blog",
+        //   link: "/blog",
+        //   leavesWebsite: false,
+        // },
+        {
+          name: "Projects",
+          link: "/projects",
+          leavesWebsite: false,
+        },
+        {
+          name: "Designs",
+          link: "/designs",
+          leavesWebsite: false,
+        },
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        {
+          name: "GitHub",
+          link: "https://github.com/JamesLiuZX",
+          icon: "/static/icons/github-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "LinkedIn",
+          link: "https://www.linkedin.com/in/james-liu-zx",
+          icon: "/static/icons/linkedin-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "Email",
+          link: "mailto:liuzixin@comp.nus.edu.sg",
+          icon: "/static/icons/mail-f.svg",
+          leavesWebsite: true,
+        },
+      ],
+    },
+  ],
+  support: {
+  },
+};
